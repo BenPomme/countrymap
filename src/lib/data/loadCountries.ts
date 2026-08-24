507b85995d3ae6485e06ec737a156f6631c6dfb2
Ben Pommeraud <benjamin.pommeraud@gmail.com>
2026-08-24 10:32:54 +0200

    Add iso3 deep-merge overlay loader for new datasets

---
+++ src/lib/data/loadCountries.ts
@@ -0,0 +1,11 @@
+import type { Country } from '@/types/country'
+import countriesData from '../../../data/countries.json'
+import overlayData from '../../../data/stat-overlay.json'
+import { mergeOverlay } from './mergeOverlay'
+
+export const countries: Country[] = mergeOverlay(
+  countriesData as Country[],
+  overlayData as Record<string, unknown>
+)
+
+export default countries
