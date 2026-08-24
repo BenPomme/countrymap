e047b4d1ddb3c50cc05fd9eee753f4a6be3a4da6
Ben Pommeraud <benjamin.pommeraud@gmail.com>
2026-08-24 10:40:50 +0200

    Load overlay map-layer configs with country data

--- src/lib/data/loadCountries.ts
+++ src/lib/data/loadCountries.ts
@@ -2,6 +2,7 @@ import type { Country } from '@/types/country'
 import countriesData from '../../../data/countries.json'
 import overlayData from '../../../data/stat-overlay.json'
 import { mergeOverlay } from './mergeOverlay'
+import '../constants/overlayVariables'
 
 export const countries: Country[] = mergeOverlay(
   countriesData as Country[],
