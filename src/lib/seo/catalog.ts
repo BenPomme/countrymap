7140e06e71c04ce720bbf681dd82323167a28e06
Ben Pommeraud <benjamin.pommeraud@gmail.com>
2026-08-24 10:29:29 +0200

    Point SEO catalog at merged countries overlay

--- src/lib/seo/catalog.ts
+++ src/lib/seo/catalog.ts
@@ -2,10 +2,10 @@ import { VARIABLES } from '@/lib/constants/variables'
 import { getNestedValue } from '@/lib/utils'
 import { countrySlug, variableSlug } from '@/lib/seo/slugs'
 import type { Country, ColorVariable } from '@/types/country'
-import countriesData from '../../../data/countries.json'
 import correlationsData from '../../../data/correlations.json'
+import { countries } from '@/lib/data/loadCountries'
 
-export const countries = countriesData as Country[]
+export { countries }
 
 export type VariableEntry = {
   id: ColorVariable
